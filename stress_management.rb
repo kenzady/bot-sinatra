e3eef04c24ef7a558c99f0e1dd811e3bdddca4d4
Kenza <kenza.dieye@gmail.com>
2018-06-14 22:03:08 +0200

    self.StressManagmenent

--- stress_management.rb
+++ stress_management.rb
@@ -1,31 +1,31 @@
 class StressManagmenent
-  def stress_mgmt_init(recipient_id)
+  def self.stress_mgmt_init(recipient_id)
     say(recipient_id, IDIOMS[:gestion_stress], GESTION_STRESS)
     as_tu_deja_essaye_resoudre
   end
 
-  def as_tu_deja_essaye_resoudre
+  def self.as_tu_deja_essaye_resoudre
     Bot.on :message do |message|
       puts "Received '#{message.inspect}' from #{message.sender}" # debug only
       sender_id = message.sender['id']
       answer = message.text.downcase
       if answer.include?("oui") || answer.include?("moyennement")
         say(sender_id, ANS_EFFICIENCY[:good], REPONSE_TERNAIRE) # Asks whether the user method was efficient
-        analyse_reponse_resolution_stress
+        StressManagmenent.analyse_reponse_resolution_stress
       elsif answer.include?("moins")
         say(sender_id, ANS_EFFICIENCY[:good], REPONSE_TERNAIRE) # Asks whether the user method was efficient
-        analyse_reponse_resolution_stress
+        StressManagmenent.analyse_reponse_resolution_stress
       elsif answer.include?("non")
         say(sender_id, ANS_EFFICIENCY[:bad], PQ_INACTION) # Asks why the user method did not work to manage his stress
-        analyse_reponse_resolution_stress
+        StressManagmenent.analyse_reponse_resolution_stress
       else
         say(sender_id, ANS_EFFICIENCY[:unknown_command], GESTION_STRESS)
-        as_tu_deja_essaye_resoudre
+        StressManagmenent.as_tu_deja_essaye_resoudre
       end
     end
   end
 
-  def analyse_reponse_resolution_stress
+  def self.analyse_reponse_resolution_stress
     if answer.include?("oui") || answer.include?("moyennement")
       say(sender_id, ANS_RESOLUTION_STRESS[:oui])
     elsif answer.include?("moins")
