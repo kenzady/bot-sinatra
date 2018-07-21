39b19009f15a530c695c8f230ea381ba3f8b0410
Kenza <kenza.dieye@gmail.com>
2018-07-21 23:35:57 +0200

    test 2

--- exercices/introduction_hexaflex.rb
+++ exercices/introduction_hexaflex.rb
@@ -18,8 +18,8 @@ class IntroductionHexaflex
       if answer.include?("présent")
         say(sender_id, REPONSE_CHOIX_DIM[:moment_present]) # réponse personalisée au choix de la dimension
         obj = MomentPresent.new
-        exercice_aleat = MomentPresent.exo_random.to_sym
-        obj.send(exercice_aleat, sender_id) # choisi aléatoirement un exercice de la dimension choisie
+        exercice_aleat = MomentPresent.exo_random+'(sender_id)'.to_sym
+        obj.send(exercice_aleat) # choisi aléatoirement un exercice de la dimension choisie
       elsif answer.include?("défusion")
         say(sender_id, REPONSE_CHOIX_DIM[:defusion])  # réponse personalisée au choix de la dimension
         # exercice_aleat = Defusion.exo_random
