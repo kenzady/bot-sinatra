88f40217f0640dac062c369f5baf1d284e1093f6
Auriane de Premilhat <adeprem@vt.edu>
2018-07-22 14:26:50 +0200

    potentially finished the exercises for moment present, granted there is no unforeseen bug

--- exercices/moment present/exos_moment_present.rb
+++ exercices/moment present/exos_moment_present.rb
@@ -2,8 +2,16 @@ require_relative 'textes_moment_present'
 
 class MomentPresent
   def self.exo_random
-    exos_moment_present = ["exo_jeu_du_detail"]
-    exos_moment_present.sample
+    exos_moment_present = ["exo_jeu_du_detail", "exo_minuteur"]
+    exercice = exos_moment_present.sample
+
+    if exercice = "exo_jeu_du_detail"
+      MomentPresent.exo_jeu_du_detail(sender_id)
+
+    elsif exercice = "exo_minuteur"
+      MomentPresent.exo_minuteur(sender_id)
+    end
+
   end
 
   def self.exo_jeu_du_detail(sender_id)
@@ -18,9 +26,86 @@ class MomentPresent
           }
         }
       )
+      sleep(30)
+      say(sender_id, JEU_DU_DETAIL[:time_up])
+      Bot.on :message do |message|
+        puts "Received '#{message.inspect}' from #{message.sender}" # debug only
+        say(sender_id, JEU_DU_DETAIL[:quel_oeil])
+        sleep(3)
+        say(sender_id, JEU_DU_DETAIL[:but_exercice])
+        sleep(5)
+        say(sender_id, JEU_DU_DETAIL[:nouvel_exercice])
+      end
     end
   end
 
-  # def self.minuteur(sender_id)
-  # end
+  def self.exo_minuteur_start(sender_id)
+    say(sender_id, MINUTEUR[:intro])
+    say(sender_id, MINUTEUR[:ready], MINUTEUR_TIME)
+    Bot.on :message do |message|
+      puts "Received '#{message.inspect}' from #{message.sender}" # debug only
+      answer = message.text.downcase
+      if answer.include?("10 minutes")
+        say(sender_id, ANS_MINUTEUR[:play])
+        sleep(600)
+        MomentPresent.exo_minuteur_suite(sender_id)
+      elsif answer.include?("30 minutes")
+        say(sender_id, ANS_MINUTEUR[:play])
+        sleep(1800)
+        MomentPresent.exo_minuteur_suite(sender_id)
+      elsif answer.include?("1 heure")
+        say(sender_id, ANS_MINUTEUR[:play])
+        sleep(3600)
+        MomentPresent.exo_minuteur_suite(sender_id)
+      elsif answer.include?("1 heure 30")
+        say(sender_id, ANS_MINUTEUR[:play])
+        sleep(5400)
+        MomentPresent.exo_minuteur_suite(sender_id)
+      elsif answer.include?("autre")
+        say(sender_id, ANS_MINUTEUR[:skip])
+        MomentPresent.exo_random(sender_id)
+      else
+        say(sender_id, MINUTEUR[:unknown_command], MINUTEUR_TIME)
+    end
+  end
+
+
+  def self.exo_minuteur_suite(sender_id)
+    say(sender_id, MINUTEUR[:time_up])
+    Bot.on :message do |message|
+      puts "Received '#{message.inspect}' from #{message.sender}" # debug only
+      answer = message.text.downcase
+      say(sender_id, MINUTEUR[:respirations])
+      message.reply(
+          attachment: {
+            type: 'image',
+            payload: {
+              url: 'https://media.giphy.com/media/krP2NRkLqnKEg/giphy.gif'
+            }
+          }
+        )
+      sleep(40)
+      say(sender_id, MINUTEUR[:feedback])
+      MomentPresent.feedback(sender_id)
+    end
+  end
+
+  def self.feedback(sender_id)
+      Bot.on :message do |message|
+        puts "Received '#{message.inspect}' from #{message.sender}" # debug only
+        answer = message.text.downcase
+        if answer.include?("beaucoup")
+          say(sender_id, ANS_FEEDBACK[:beaucoup])
+          MomentPresent.exo_random(sender_id)
+        elsif answer.include?("peu")
+          say(sender_id, ANS_FEEDBACK[:un_peu])
+          MomentPresent.exo_random(sender_id)
+        elsif answer.include?("pas")
+          say(sender_id, ANS_FEEDBACK[:pas_du_tout])
+          MomentPresent.exo_random(sender_id)
+        else
+          say(sender_id, MINUTEUR[:unknown_command], MINUTEUR[:feedback])
+      end
+  end
+
 end
--- exercices/moment present/textes_moment_present.rb
+++ exercices/moment present/textes_moment_present.rb
@@ -2,7 +2,69 @@ JEU_DU_DETAIL = {
   intro:
   "Let's go!
   Cet exercice s'appelle 'Le jeu du détail'.Je vais te montrer une image, et tu auras 30 secondes pour décrire le plus de détails possible. 👁🖼" ,
-  ready: "Est-tu prêt ? Tu as 30 sec dès que tu push le bouton !"
+  ready:
+  "Est-tu prêt ? Tu as 30 sec dès que tu push le bouton !",
+  time_up:
+  "Time's up ! Décris moi ce que tu as vu 😃",
+  quel_oeil:
+  "Quel oeil ! J'en avais manqué plein !",
+  but_exercice:
+  "Cet exercice est fait pour te faire travailler ta concentration et donc à faire le vide autour de toi pendant un court instant. J'espère que tu l'as aimé.",
+  nouvel_exercice:
+  "Maintenant que celui-ci est terminé, as-tu envie d'en faire un autre?"
+}
+
+
+MINUTEUR = {
+  intro:
+  "C'est parti !
+  Cet exercice s'appelle 'Le Minuteur'. Son but est de te redonner l'habitude d'être en contact avec toi-même et de vivre en pleine conscience." ,
+  ready:
+  "Pour se faire, tu vas définir une durée, et à la fin de celle-ci, je te préviendrai que c'est terminé, et tu devras noter ce que tu étais en train de faire à cet instant. ⏰🗒",
+  time_up:
+  "Dring Dring, Note maintenant ce que tu étais en train de faire quand l'alarme a sonné.",
+  respirations:
+  "Super ! Maintenant prends 4 grandes respirations, sans penser à ce que tu étais en train de faire puis expire. Tu peux suivre ce gif pour t'aider."
+  feedback:
+  "Voilà, c'est terminé ! As-tu apprécié cet exercice?",
+  unknown_command:
+  "Je ne comprends pas ce que tu as dit, peux-tu appuyer sur un bouton s'il te plait?"
+}
+
+
+MINUTEUR_TIME = [
+  {
+    content_type: 'text',
+    title: '10 minutes',
+    payload: 'DIX_MINUTES'
+  },
+  {
+    content_type: 'text',
+    title: '30 minutes',
+    payload: 'TRENTE_MINUTES'
+  },
+  {
+    content_type: 'text',
+    title: '1 heure',
+    payload: 'UNE_HEURE'
+  },
+  {
+    content_type: 'text',
+    title: '1 heure 30',
+    payload: 'UNE_HEURE_TRENTE'
+  },
+  {
+    content_type: 'text',
+    title: 'Faire un autre exercice',
+    payload: 'SKIP_EXERCICE'
+  }
+]
+
+ANS_MINUTEUR = {
+  play:
+  "C'est noté!  A tout à l'heure ! 👋🏼" ,
+  skip:
+  "Comme tu veux 😶 Je te propose alors un autre exercice, en espérant que celui-là te plaise !"
 }
 
 START_EXERCISE = [
@@ -23,3 +85,33 @@ START_EXERCISE = [
   }
 ]
 
+FEEDBACK= [
+  {
+    content_type: 'text',
+    title: 'Beaucoup',
+    payload: 'BEAUCOUP'
+  },
+  {
+    content_type: 'text',
+    title: "Un petit peu",
+    payload: 'UN_PEU'
+  },
+  {
+    content_type: 'text',
+    title: 'Pas du tout',
+    payload: 'PAS_DU_TOUT'
+  }
+]
+
+ANS_FEEDBACK = {
+  beaucoup:
+  "Génial 😎 ! Sache que cet exercice devient de plus en plus facile à force de répétition. Hâte de réitérer l'expérience avec toi ! 😉
+Que veux tu faire maintenant?" ,
+  un_peu:
+  "Tant mieux ! Sache que cet exercice devient de plus en plus facile à force de répétition.Hâte de réitérer l'expérience avec toi ! 😉
+Que veux-tu faire maintenant?",
+  pas_du_tout:
+  "J'en suis navré 😞😔 Sache que cet exercice devient de plus en plus facile à force de répétition. J'espère que tu l'apprécieras plus la prochaine fois !
+Que veux tu faire maintenant?"
+
+}
