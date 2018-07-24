3ab02079231b40380696d470a3ee0643579f8305
Kenza <kenza.dieye@gmail.com>
2018-07-24 23:23:01 +0200

    rajoute les 4 arguments dans dernière ligne methode GeneraleExo.nouvel_exercice

--- exercices/gestion_generale_exos.rb
+++ exercices/gestion_generale_exos.rb
@@ -1,4 +1,4 @@
-'moment_present/exos_moment_present'
+require_relative 'moment_present/exos_moment_present'
 #methode except pour exclure l'exo déjà effectué de la liste des exos lorsque le user veut en faire un nouveau
 class Array
   def except(value)
@@ -23,21 +23,21 @@ class GeneraleExos
   end
 
 def self.nouvel_exercice(sender_id, dim, exos_dim, exo_fait)
-  Bot.on :message do |message| #recupere la réponse de l'utilisateur
-      puts "Received '#{message.inspect}' from #{message.sender}" # debug only
-      answer = message.text.downcase
-      if answer.include?("oui")  #utilisateur veut faire un autre exercice
-        exos_dim = exos_dim.except(exo_fait)
-        GeneraleExos.exo_random(sender_id, exos_dim, dim) #nouvel exercice random
-      elsif answer.include?("non")
-        say(sender_id, NEW_EXO[:au_revoir]) #pas de nouvel exercice, on dit au revoir
-      elsif answer.include?("changer") #utilisateur veut changer de dimension
-        IntroductionHexaflex.presentation_hexaflex(sender_id) #redirige vers l'explication des exos
-      else
-        say(sender_id, NEW_EXO[:unknown_command], NOUVEL_EXERCICE) #pas compris, on redemande
-        GeneraleExos.nouvel_exercice
-      end
+  Bot.on :message do |message| # Recupere la réponse de l'utilisateur
+    puts "Received '#{message.inspect}' from #{message.sender}" # debug only
+    answer = message.text.downcase
+    if answer.include?("oui")  # Utilisateur veut faire un autre exercice
+      exos_dim = exos_dim.except(exo_fait) # exclue l'exo déjà fait
+      GeneraleExos.exo_random(sender_id, exos_dim, dim) #nouvel exercice random parmi ceux non faits
+    elsif answer.include?("non") # Si le user ne veut pas faire de new exo
+      say(sender_id, NEW_EXO[:au_revoir]) # On dit au revoir
+    elsif answer.include?("changer") # Utilisateur veut changer de dimension
+      IntroductionHexaflex.presentation_hexaflex(sender_id) #redirige vers l'explication des exos
+    else
+      say(sender_id, NEW_EXO[:unknown_command], NOUVEL_EXERCICE) #pas compris, on redemande
+      GeneraleExos.nouvel_exercice(sender_id, MomentPresent, exos_moment_present, "exo_jeu_du_detail")
     end
+  end
 end
 
 end
