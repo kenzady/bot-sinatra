e46e11d8f4ecff05ab3ed0e3944ba1084f0179ba
Auriane de Premilhat <adeprem@vt.edu>
2018-07-25 18:45:21 +0200

    started the architecture of defusion

--- exercices/defusion/exos_defusion.rb
+++ exercices/defusion/exos_defusion.rb
@@ -2,13 +2,20 @@ require_relative 'textes_defusion'
 
 class Defusion
   def self.exo_random
-    exos_defusion = []
-    exos_defusion.sample
+    exos_defusion = ["exo_chocolat_chocolat", "exo_jeu_du_mais"]
+    exercice = exos_defusion.sample
+
+    if exercice == "exo_chocolat_chocolat"
+      exos_defusion[1] = "exo_jeu_du_mais"
+      Defusion.exo_chocolat_chocolat(sender_id, exos_defusion)
+    elsif exercice == "exo_jeu_du_mais"
+      Defusion.exo_jeu_du_mais(sender_id)
+    end
   end
 
-  def self.exo_1
+  def self.exo_chocolat_chocolat
   end
 
-  def self.exo_2
+  def self.exo_jeu_du_mais
   end
 end
