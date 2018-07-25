8e0e88746925d1cd05b65c5ee756c13bdfce5d60
Auriane de Premilhat <adeprem@vt.edu>
2018-07-25 18:56:42 +0200

    added texts for chocolat chocolat

---
+++ exercices/defusion/textes_defusion.rb
@@ -0,0 +1,36 @@
+CHOCOLAT_CHOCOLAT= {
+  intro:
+  "C'est parti !
+Pour cet exercice je vais tout d'abord te demander de prononcer lentement le mot 'chocolat' et d'observer ce qu'il éveille en toi",
+  intro_bis:
+  "Moi, quand je le prononce, je peux pratiquement sentir l'odeur qui me monte aux narines, sentir la réaction de mes papilles quand je goûte du chocolat, ...",
+  repetition:
+  "Maintenant, répete le mot 'chocolat' à haute voix, sans interruption pendant une minute (je te dirai quand t'arrêter)",
+  arret_repetition:
+  "Après un si grand nombre de répétitions, le mot chocolat te paraît étrange, non ? Tu ne penses plus au sens du mot mais plutôt au son étrange que ta voix produit.
+Ce n’est qu’un mot mais les mots ont le pouvoir de porter avec eux les caractéristiques des choses qu’ils désignent et ils ne cessent de le faire que si nous mettons en œuvre des techniques visant à ne plus les entendre que comme les sons qu’ils sont en réalité."
+  debut_repetition_souffrance:
+  "Maintenant, je vais te demander de penser à un mot, ou bien une phrase qui te fait souffrir. C'est bon ?"
+  fin_exercice:
+  "Voilà, c'est terminé. Cet exercice permet de se détacher du sens des mots, et peut aider à réduire la souffrance associée à certaines phrases.
+C'est un exercice que tu peux répéter par toi même aussi souvent que tu le désires, et j'espère que tu l'as apprécié ! "
+}
+
+
+ANS_CHOCOLAT_CHOCOLAT = [
+  {
+    content_type: 'text',
+    title: "C'est bon",
+    payload: 'OK'
+  },
+  {
+    content_type: 'text',
+    title: "Pas envie de faire cet exo",
+    payload: 'CHANGE_EXO'
+  },
+  {
+    content_type: 'text',
+    title: 'Changer de dimension',
+    payload: 'ANOTHER_DIMENSION'
+  }
+]
