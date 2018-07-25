24f0c4e6180a5effb43340f0eac95dff3cbfc33a
Auriane de Premilhat <adeprem@vt.edu>
2018-07-25 19:18:52 +0200

    rajout de virgules oubliées

--- exercices/defusion/exos_defusion.rb
+++ exercices/defusion/exos_defusion.rb
@@ -13,7 +13,17 @@ class Defusion
     end
   end
 
-  def self.exo_chocolat_chocolat
+  def self.exo_chocolat_chocolat(sender_id, exos_defusion)
+    say(sender_id, CHOCOLAT_CHOCOLAT[:intro]) #intro de l'exercice, chocolat lentement
+    #insérer une pause
+    say(sender_id, CHOCOLAT_CHOCOLAT[:intro_bis]) # bot dit ce qu'il ressent avec chocolat
+    say(sender_id, CHOCOLAT_CHOCOLAT[:repetition]) #demande de répéter le mot chocolat pdt 1 minute
+    #insérer une pause d'1 a 2 minutes
+    say(sender_id, CHOCOLAT_CHOCOLAT[:arret_repetition])
+    say(sender_id, CHOCOLAT_CHOCOLAT[:debut_repetition_souffrance]) #demande de répéter le mot qui fait souffrir pdt 1 minute
+    #insérer une pause d'1 a 2 minutes
+    say(sender_id, CHOCOLAT_CHOCOLAT[:fin_exercice]) #explique le but de l'exercice
+
   end
 
   def self.exo_jeu_du_mais
