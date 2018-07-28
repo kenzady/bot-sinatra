5ba007100c32096fe4ae35002caf80e2068aab67
Kenza <kenza.dieye@gmail.com>
2018-07-29 01:06:22 +0200

    aidre require relative

--- exercices/defusion/exos_defusion.rb
+++ exercices/defusion/exos_defusion.rb
@@ -1,5 +1,5 @@
 require_relative 'textes_defusion'
-require_relative '../gestion_generale_exos'
+require_relative '../structure unique/gestion_generale_exos'
 
 class Defusion
   def self.exo_random(sender_id)
