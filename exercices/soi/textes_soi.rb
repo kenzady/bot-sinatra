0f720f42ed0902d67f01b065232bfd2a40053355
Auriane de Premilhat <adeprem@vt.edu>
2018-07-28 12:40:44 +0200

    added architecture for soi_observateur

---
+++ exercices/soi/textes_soi.rb
@@ -0,0 +1,50 @@
+SOI_OBSERVATEUR = {
+  ready:
+  "L'exercice que je te propose s'appelle 'Le soi observateur' et c'est un exercice audio. Ça te tente ?",
+  intro:
+  "C'est parti alors, le voici !" ,
+  nouvel_exercice:
+  "Maintenant que nous avons terminé, as-tu envie d'en faire un autre ?",
+  unknown_command:
+  "Je ne comprends pas ce que tu as dit, peux-tu appuyer sur un bouton s'il te plait?",
+  au_revoir:
+  "Pas de souci, en espérant que tu reviennes bientôt !"
+}
+
+START_EXERCISE = [
+  {
+    content_type: 'text',
+    title: 'GO!',
+    payload: 'GO'
+  },
+  {
+    content_type: 'text',
+    title: "Changer d'exo",
+    payload: 'CHANGE_EXO'
+  },
+  {
+    content_type: 'text',
+    title: 'Autre dimension',
+    payload: 'ANOTHER_DIMENSION'
+  }
+]
+
+
+
+NOUVEL_EXERCICE  = [
+  {
+    content_type: 'text',
+    title: 'Oui',
+    payload: 'OUI'
+  },
+  {
+    content_type: 'text',
+    title: "Non",
+    payload: 'NON'
+  },
+  {
+    content_type: 'text',
+    title: 'Changer de thème',
+    payload: 'CHANGER_THEME'
+  }
+]
