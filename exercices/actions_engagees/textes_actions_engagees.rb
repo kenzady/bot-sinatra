b51a8a921f19bb8c7cb084008c28e3045cd67e13
Auriane de Premilhat <adeprem@vt.edu>
2018-07-28 17:04:36 +0200

    added texts for actions engagées

---
+++ exercices/actions_engagees/textes_actions_engagees.rb
@@ -0,0 +1,64 @@
+DECOUPAGE = {
+  ready:
+  "L'exercice que je te propose s'appelle '---' et c'est un exercice audio. Ça te tente ?",
+  intro:
+  "C'est parti alors, le voici !" ,
+  nouvel_exercice:
+  "Maintenant que nous avons terminé, as-tu envie d'en faire un autre ?",
+  unknown_command:
+  "Je ne comprends pas ce que tu as dit, peux-tu appuyer sur un bouton s'il te plait?",
+  au_revoir:
+  "Pas de souci, en espérant que tu reviennes bientôt !"
+}
+
+MESURE = {
+  ready:
+  "L'exercice que je te propose s'appelle '---' Ça te tente ?",
+  intro:
+  "C'est parti alors, le voici !" ,
+  nouvel_exercice:
+  "Maintenant que nous avons terminé, as-tu envie d'en faire un autre ?",
+  unknown_command:
+  "Je ne comprends pas ce que tu as dit, peux-tu appuyer sur un bouton s'il te plait?",
+  au_revoir:
+  "Pas de souci, en espérant que tu reviennes bientôt !"
+}
+
+
+START_EXERCISE = [
+  {
+    content_type: 'text',
+    title: 'GO!',
+    payload: 'GO'
+  },
+  {
+    content_type: 'text',
+    title: "Changer d'exo",
+    payload: 'CHANGE_EXO'
+  },
+  {
+    content_type: 'text',
+    title: 'Autre dimension',
+    payload: 'ANOTHER_DIMENSION'
+  }
+]
+
+
+
+NOUVEL_EXERCICE  = [
+  {
+    content_type: 'text',
+    title: 'Oui',
+    payload: 'OUI'
+  },
+  {
+    content_type: 'text',
+    title: "Non",
+    payload: 'NON'
+  },
+  {
+    content_type: 'text',
+    title: 'Changer de thème',
+    payload: 'CHANGER_THEME'
+  }
+]
