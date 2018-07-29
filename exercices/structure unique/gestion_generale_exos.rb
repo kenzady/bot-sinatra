b9b37d9ca628f9488843de2725a02632f9c4d997
Auriane de Premilhat <adeprem@vt.edu>
2018-07-29 19:48:53 +0200

    removed the self from exo

--- exercices/structure unique/gestion_generale_exos.rb
+++ exercices/structure unique/gestion_generale_exos.rb
@@ -17,7 +17,8 @@ class GeneraleExos
     exo_aleat = exos_dim.sample #selectionne un exercice au hasard
     exos_dim.each do |exo|
       if exo_aleat == exo && exo_aleat != "exo_minuteur" # Si l'exercice choisi aléatoirement match avec un exo de l'array d'exos, on balance l'exo en question
-        exo = "self.#{exo}"
+        #exo = "self.#{exo}"
+        exo = "#{exo}"
         dim.send(exo, sender_id)
       else
         MomentPresent.exo_minuteur_start(sender_id, exos_dim)
