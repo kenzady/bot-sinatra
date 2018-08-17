6b90640c3bdd0f5c02710409ce79794d0b49f8d0
Kenza <kenza.dieye@gmail.com>
2018-08-18 00:01:52 +0200

    models

--- app.rb
+++ app.rb
@@ -5,14 +5,22 @@ require 'rubygems'
 require 'sinatra'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'postgresql',
-  :host => 'localhost',
-  :username => "kenza",
-  :password => "ignilifeprojectacek",
-  :database =>  'ignilifeproject'
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => 'postgresql',
+#   :host => 'localhost',
+#   :username => "kenza",
+#   :password => "ignilifeprojectacek",
+#   :database =>  'ignilifeproject'
+# )
 # NOTE: ENV variables should be set directly in terminal for testing on localhost
+
+configure :development do
+  set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'ignilifeproject', pool: 2, username: 'kenza', password: 'ignilifeprojectacek'}
+end
+
+configure :production do
+  set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'ignilifeproject', pool: 2, username: 'kenza', password: 'ignilifeprojectacek'}
+end
 
 # Talk to Facebook
 get '/webhook' do
